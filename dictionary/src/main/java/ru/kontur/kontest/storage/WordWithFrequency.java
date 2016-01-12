10b696ead9520cb08b9bbaf5f8312c1921d8ef16
danufriev <dmitryanufriev@gmail.com>
2016-01-12 23:10:15 +0500

    Переименовал переменную

--- dictionary/src/main/java/ru/kontur/kontest/storage/WordWithFrequency.java
+++ dictionary/src/main/java/ru/kontur/kontest/storage/WordWithFrequency.java
@@ -11,13 +11,13 @@ public class WordWithFrequency implements Comparable<WordWithFrequency> {
 	}
 
 	public int compareTo(WordWithFrequency wordWithFrequency) {
-		int comparisonResult = wordWithFrequency.frequency - frequency;
+		int frequenciesComparisonResult = wordWithFrequency.frequency - frequency;
 		
-		if (comparisonResult == 0) { // Частоты одинаковые, сравниваем слова
+		if (frequenciesComparisonResult == 0) { // Частоты одинаковые, сравниваем слова
 			return word.compareTo(wordWithFrequency.word);
 		}
 		
-		return comparisonResult;
+		return frequenciesComparisonResult;
 	}
 
 	@Override
