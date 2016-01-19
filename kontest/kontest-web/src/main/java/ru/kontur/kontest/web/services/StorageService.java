152037be4a4cfcf239ce4ae6f6389b7aae24aee6
danufriev <danufriev@skbkontur.ru>
2016-01-20 01:55:29 +0500

    Читаем файл с тестовыми данными после создания StorageService.

--- kontest/kontest-web/src/main/java/ru/kontur/kontest/web/services/StorageService.java
+++ kontest/kontest-web/src/main/java/ru/kontur/kontest/web/services/StorageService.java
@@ -1,11 +1,19 @@
 package ru.kontur.kontest.web.services;
 
+import java.io.InputStream;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 import ru.kontur.kontest.storages.HashMapStorage;
 import ru.kontur.kontest.storages.Storage;
 import ru.kontur.kontest.words.WordWithFrequency;
 
-public class StorageService {
+public class StorageService implements InitializingBean {
 
+	private Storage storage;
+	
 	public Storage getStorage() {
 		HashMapStorage hashMapStorage = new HashMapStorage(10, 10);
 		
@@ -16,6 +24,14 @@ public class StorageService {
 		hashMapStorage.put(new WordWithFrequency("sakura", 3));
 		
 		return hashMapStorage;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		Resource resource = new ClassPathResource("test.in");
+		try(InputStream resourceInputStream = resource.getInputStream()) {
+			System.out.println(resourceInputStream.available());
+		}
 	}
 
 }
