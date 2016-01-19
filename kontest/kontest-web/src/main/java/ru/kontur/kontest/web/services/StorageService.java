51afb413e58821a1e7be6fbef2daa15415769a65
danufriev <danufriev@skbkontur.ru>
2016-01-19 17:18:51 +0500

    Вставил тестовые данные для web части.

--- kontest/kontest-web/src/main/java/ru/kontur/kontest/web/services/StorageService.java
+++ kontest/kontest-web/src/main/java/ru/kontur/kontest/web/services/StorageService.java
@@ -2,11 +2,20 @@ package ru.kontur.kontest.web.services;
 
 import ru.kontur.kontest.storages.HashMapStorage;
 import ru.kontur.kontest.storages.Storage;
+import ru.kontur.kontest.words.WordWithFrequency;
 
 public class StorageService {
 
 	public Storage getStorage() {
-		return new HashMapStorage(10, 10);
+		HashMapStorage hashMapStorage = new HashMapStorage(10, 10);
+		
+		hashMapStorage.put(new WordWithFrequency("kare", 10));
+		hashMapStorage.put(new WordWithFrequency("kanojo", 20));
+		hashMapStorage.put(new WordWithFrequency("karetachi", 10));
+		hashMapStorage.put(new WordWithFrequency("korosu", 7));
+		hashMapStorage.put(new WordWithFrequency("sakura", 3));
+		
+		return hashMapStorage;
 	}
 
 }
