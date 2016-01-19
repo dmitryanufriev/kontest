74bba814e0cfdf9abb9b85f523a7a3f64afd1eaf
danufriev <danufriev@skbkontur.ru>
2016-01-19 16:39:25 +0500

    Добавил инциализацю сервисов (сервиса хранилища).

--- kontest/kontest-web/src/main/java/ru/kontur/kontest/web/configuration/ApplicationContext.java
+++ kontest/kontest-web/src/main/java/ru/kontur/kontest/web/configuration/ApplicationContext.java
@@ -3,7 +3,7 @@ package ru.kontur.kontest.web.configuration;
 import org.springframework.context.annotation.*;
 
 @Configuration
-@Import({ WebContext.class })
+@Import({ WebContext.class, ServicesContext.class })
 public class ApplicationContext {
 
 }
---
+++ kontest/kontest-web/src/main/java/ru/kontur/kontest/web/configuration/ServicesContext.java
@@ -0,0 +1,16 @@
+package ru.kontur.kontest.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import ru.kontur.kontest.web.services.StorageService;
+
+@Configuration
+public class ServicesContext {
+
+	@Bean
+	public StorageService storageService() {
+		return new StorageService();
+	}
+	
+}
