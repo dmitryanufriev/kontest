22f8fd2779844e80694c9f31e4289d45bf45a245
danufriev <danufriev@skbkontur.ru>
2016-01-20 15:27:07 +0500

    Реформат.

--- kontest/console/src/main/java/ru/kontur/kontest/console/Application.java
+++ kontest/console/src/main/java/ru/kontur/kontest/console/Application.java
@@ -1,10 +1,5 @@
 package ru.kontur.kontest.console;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Collection;
-
 import ru.kontur.kontest.io.InputReader;
 import ru.kontur.kontest.io.OutputWriter;
 import ru.kontur.kontest.io.listeners.TestDataListener;
@@ -13,49 +8,53 @@ import ru.kontur.kontest.storages.Storage;
 import ru.kontur.kontest.words.Prefix;
 import ru.kontur.kontest.words.WordWithFrequency;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+
 public class Application {
 
-	private static final int WORDS_COUNT = 10;
+  public static void main(String[] args) {
+    Application application = new Application();
+    try {
+      application.execute(System.in, System.out);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	public static void main(String[] args) {
-		Application application = new Application();
-		try {
-			application.execute(System.in, System.out);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void execute(InputStream inputStream, final OutputStream outputStream) throws IOException {
-		InputReader inputReader = new InputReader();
-		inputReader.readFrom(inputStream, new TestDataListener() {
-			private final OutputWriter outputWriter = new OutputWriter();
-			private Storage storage;
-			
-			@Override
-			public void wordsCount(int count) {
-				storage = new HashMapStorage(count, WORDS_COUNT);
-			}
-			
-			@Override
-			public void prefixesCount(int count) {
-			}
-			
-			@Override
-			public void nextWord(WordWithFrequency wordWithFrequency) {
-				storage.put(wordWithFrequency);				
-			}
-			
-			@Override
-			public void nextPrefix(Prefix prefix) {
-				Collection<WordWithFrequency> foundWords = storage.searchWordsBy(prefix);
-				try {
-					outputWriter.writeWordsToStream(foundWords, outputStream);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	
+  public void execute(InputStream inputStream, final OutputStream outputStream) throws IOException {
+    InputReader inputReader = new InputReader();
+    inputReader.readFrom(inputStream, new TestDataListener() {
+      private final OutputWriter outputWriter = new OutputWriter();
+      private Storage storage;
+
+      @Override
+      public void wordsCount(int count) {
+        storage = new HashMapStorage(count, WORDS_COUNT);
+      }
+
+      @Override
+      public void prefixesCount(int count) {
+      }
+
+      @Override
+      public void nextWord(WordWithFrequency wordWithFrequency) {
+        storage.put(wordWithFrequency);
+      }
+
+      @Override
+      public void nextPrefix(Prefix prefix) {
+        Collection<WordWithFrequency> foundWords = storage.searchWordsBy(prefix);
+        try {
+          outputWriter.writeWordsToStream(foundWords, outputStream);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+  
+  private static final int WORDS_COUNT = 10;
 }
--- kontest/console/src/test/java/ru/kontur/kontest/console/ApplicationTest.java
+++ kontest/console/src/test/java/ru/kontur/kontest/console/ApplicationTest.java
@@ -1,49 +1,51 @@
 package ru.kontur.kontest.console;
 
-import static org.junit.Assume.*;
+import static org.junit.Assume.assumeTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.junit.*;
-import org.junit.rules.Timeout;
-
 public class ApplicationTest {
-	
-	@Rule
-	public Timeout tenSeconds = new Timeout(10000);
-	
-	private static final String FILE_NAME = "test.in";
-	private static final boolean RUN_ACCEPTANCE_TEST = false;
-	private InputStream in;
 
-	@Before
-	public void setUp() {
-		ClassLoader classLoader = getClass().getClassLoader();
-		in = classLoader.getResourceAsStream(FILE_NAME);
-	}
-	
-	@After
-	public void tearDown() {
-		try {
-			in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void acceptanceTest() {
-		assumeTrue(RUN_ACCEPTANCE_TEST);
-		
-		Application application = new Application();
-		
-		try {
-			application.execute(in, new ByteArrayOutputStream());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+  @Rule
+  public Timeout tenSeconds = new Timeout(10000);
+
+  private static final String FILE_NAME = "test.in";
+  private static final boolean RUN_ACCEPTANCE_TEST = false;
+  private InputStream in;
+
+  @Before
+  public void setUp() {
+    ClassLoader classLoader = getClass().getClassLoader();
+    in = classLoader.getResourceAsStream(FILE_NAME);
+  }
+
+  @After
+  public void tearDown() {
+    try {
+      in.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void acceptanceTest() {
+    assumeTrue(RUN_ACCEPTANCE_TEST);
+
+    Application application = new Application();
+
+    try {
+      application.execute(in, new ByteArrayOutputStream());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
