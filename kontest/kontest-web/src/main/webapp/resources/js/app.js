933711cce1bd6f2f5b7c9903f883fa2f3c43f9b6
danufriev <danufriev@skbkontur.ru>
2016-01-21 18:39:42 +0500

    Исключил модуль автодополнения из глобальной области видимости.

--- kontest/kontest-web/src/main/webapp/resources/js/app.js
+++ kontest/kontest-web/src/main/webapp/resources/js/app.js
@@ -1,31 +1,35 @@
-var autocomplete = angular.module('autocomplete', []);
+(function() {
 
-autocomplete.controller('AutocompleteCtrl', function($scope, wordsSource) {
-	$scope.prefix = undefined;
-	$scope.words = [];
+	var autocomplete = angular.module('autocomplete', []);
 
-	$scope.init = function(autocompleteUrl) {
-		$scope.autcompleteUrl = autocompleteUrl;
-	};
-	
-	$scope.onedit = function() {
+	autocomplete.controller('AutocompleteCtrl', function($scope, wordsSource) {
+		$scope.prefix = undefined;
 		$scope.words = [];
 
-		wordsSource.get($scope.autcompleteUrl + $scope.prefix).then(function(foundWords) {
-			for (var i = 0; i < foundWords.length; i ++) {
-				$scope.words.push(foundWords[i]);
-			}
-		});
-	};
-	
-});
+		$scope.init = function(autocompleteUrl) {
+			$scope.autcompleteUrl = autocompleteUrl;
+		};
+		
+		$scope.onedit = function() {
+			$scope.words = [];
 
-autocomplete.factory('wordsSource', function($http) {
-	return {
-		get : function(url) {
-			return $http.get(url).then(function(response) {
-				return response.data;
+			wordsSource.get($scope.autcompleteUrl + $scope.prefix).then(function(foundWords) {
+				for (var i = 0; i < foundWords.length; i ++) {
+					$scope.words.push(foundWords[i]);
+				}
 			});
-		}
-	};
-});
+		};
+		
+	});
+
+	autocomplete.factory('wordsSource', function($http) {
+		return {
+			get : function(url) {
+				return $http.get(url).then(function(response) {
+					return response.data;
+				});
+			}
+		};
+	});
+
+})();
